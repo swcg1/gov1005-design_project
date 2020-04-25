45e4cd73047e38931ba089ed42514b34bd19675a
swcg1 <stephaniewcheng@gmail.com>
2020-04-25 18:00:18 +0800

    half done with salary/satisfaction tab

--- design_project/app.R
+++ design_project/app.R
@@ -40,11 +40,33 @@ ui <- fluidPage(
                      plotOutput("tab1")
                  )
              )),
+    tabPanel("By Salary and Job Satisfaction",
+             sidebarLayout(
+                 sidebarPanel(
+                     selectInput("y_var",
+                                 label = "Choose a metric to investigate",
+                                 choices = list("Salary", "Job Satisfaction"),
+                                 selected = "Salary"),
+                     selectInput("x_var",
+                                 label = "Choose a variable to investigate",
+                                 choices = list("Age",
+                                                "Gender",
+                                                "Career Duration",
+                                                "Organization Size",
+                                                "Department Size"))
+                 ),
+                 mainPanel(
+                     tabsetPanel(type = "tabs",
+                                 tabPanel("2017",
+                                          plotOutput("salary_age_2017")),
+                                 tabPanel("2019",
+                                          plotOutput("salary_age_2019")))
+                 )
+             )
+             ),
+    tabPanel("Predictor"
+             ),
     tabPanel("By Location"
-             ),
-    tabPanel("By Salary"
-             ),
-    tabPanel("By Satisfaction"
              )
     )
 )
@@ -60,6 +82,123 @@ server <- function(input, output) {
             ggplot(aes(age, n)) +
             geom_bar(stat = "identity")
     )
+    
+    salary_satisfaction_react_2017 <- reactive({
+        if(input$x_var == "Age"){
+            summarized <- census_model_joined %>%
+                group_by(age_group) %>%
+                filter(year == 2017) %>%
+                summarize(mean_salary = mean(salary)) %>%
+                ggplot(aes(age_group, mean_salary)) +
+                geom_col(aes(fill = age_group))
+        }
+        else if(input$x_var == "Gender"){
+            summarized <- census_model_joined %>%
+                group_by(gender) %>%
+                filter(year == 2017) %>%
+                summarize(mean_salary = mean(salary)) %>%
+                ggplot(aes(gender, mean_salary)) +
+                geom_col(aes(fill = gender))
+        }
+        else if(input$x_var == "Career Duration"){
+            summarized <- census_model_joined %>%
+                group_by(career_duration) %>%
+                filter(year == 2017) %>%
+                summarize(mean_salary = mean(salary)) %>%
+                ggplot(aes(career_duration, mean_salary)) +
+                geom_col(aes(fill = career_duration))
+        }
+        else if(input$x_var == "Organization Size"){
+            summarized <- census_model_joined %>%
+                group_by(org_size) %>%
+                filter(year == 2017,
+                       !org_size == "") %>%
+                summarize(mean_salary = mean(salary)) %>%
+                ggplot(aes(org_size, mean_salary)) +
+                geom_col(aes(fill = org_size))
+        }
+        else if(input$x_var == "Department Size"){
+            summarized <- census_model_joined %>%
+                group_by(department_size) %>%
+                filter(year == 2017,
+                       !department_size == "") %>%
+                summarize(mean_salary = mean(salary)) %>%
+                ggplot(aes(department_size, mean_salary)) +
+                geom_col(aes(fill = department_size))
+        }
+    })
+    
+    salary_satisfaction_react_2019 <- reactive({
+        if(input$x_var == "Age"){
+            summarized <- census_model_joined %>%
+                group_by(age_group) %>%
+                filter(year == 2019) %>%
+                summarize(mean_salary = mean(salary)) %>%
+                ggplot(aes(age_group, mean_salary)) +
+                geom_col(aes(fill = age_group))
+        }
+        else if(input$x_var == "Gender"){
+            summarized <- census_model_joined %>%
+                group_by(gender) %>%
+                filter(year == 2019) %>%
+                summarize(mean_salary = mean(salary)) %>%
+                ggplot(aes(gender, mean_salary)) +
+                geom_col(aes(fill = gender))
+        }
+        else if(input$x_var == "Career Duration"){
+            summarized <- census_model_joined %>%
+                group_by(career_duration) %>%
+                filter(year == 2019) %>%
+                summarize(mean_salary = mean(salary)) %>%
+                ggplot(aes(career_duration, mean_salary)) +
+                geom_col(aes(fill = career_duration))
+        }
+        else if(input$x_var == "Organization Size"){
+            summarized <- census_model_joined %>%
+                group_by(org_size) %>%
+                filter(year == 2019,
+                       !is.na(org_size)) %>%
+                summarize(mean_salary = mean(salary)) %>%
+                ggplot(aes(org_size, mean_salary)) +
+                geom_col(aes(fill = org_size))
+        }
+        else if(input$x_var == "Department Size"){
+            summarized <- census_model_joined %>%
+                group_by(department_size) %>%
+                filter(year == 2019) %>%
+                summarize(mean_salary = mean(salary)) %>%
+                ggplot(aes(department_size, mean_salary)) +
+                geom_col(aes(fill = department_size))
+        }
+    })
+    
+    output$salary_age_2017 <- renderPlot({
+        bar_palette <- c('#6981e6', '#6f8ce6', '#7597e7', '#7aa2e7', '#7eade7', '#82b9e7', '#86c4e7', '#89cfe7', '#8cdbe6')
+        
+        salary_age <- salary_satisfaction_react_2017() +
+            scale_fill_manual(values = bar_palette, name = input$x_var) +
+            theme_classic() +
+            labs(x = input$x_var,
+                 y = "Mean Salary",
+                 title = paste("Salary by", input$x_var),
+                 subtitle = "Of Designers in the US in 2017")
+        
+        salary_age
+    })
+    
+    output$salary_age_2019 <- renderPlot({
+        bar_palette <- c('#ff6998', '#ff7499', '#ff7f99', '#ff889a', '#ff929a', '#ff9b9a', '#ffa49b', '#ffac9b', '#ffb49b')
+        
+        salary_age <- salary_satisfaction_react_2019() +
+            scale_fill_manual(values = bar_palette, name = input$x_var) +
+            theme_classic() +
+            labs(x = input$x_var,
+                 y = "Mean Salary",
+                 title = paste("Salary by", input$x_var),
+                 subtitle = "Of Designers in the US in 2019")
+        
+        salary_age
+    })
 }
 
 # Run the application 
